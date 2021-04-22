1f8b38cdb8303ccb354fa7d3505992e09562e1c5
Maksymilian Babarowski <maksymilian.babarowski@gmail.com>
2021-04-22 23:44:58 +0200

    Reorganized project by splitting back-end to two stacks containing persistent resources (Cognito, DynamoDB) and often modified resources (Lambda)

--- services/api/microblog/utils/parser.py
+++ services/api/microblog/utils/parser.py
@@ -4,6 +4,7 @@ from typing import Optional, Union, Dict, Any, Type, TypeVar
 
 from aws_lambda_powertools.utilities.data_classes import APIGatewayProxyEventV2
 from aws_lambda_powertools.utilities.parser import BaseEnvelope
+from requests_toolbelt.multipart import decoder
 
 from microblog.models.base_model import BaseModel
 
@@ -13,8 +14,16 @@ Model = TypeVar("Model", bound=BaseModel)
 class ApiGatewayProxyV2Envelope(BaseEnvelope):
     def parse(self, data: Optional[Union[Dict[str, Any], Any]], model: Type[Model]) -> Optional[Model]:
         parsed_envelope = APIGatewayProxyEventV2(data)
+        content_type = parsed_envelope.headers.get('Content-Type', 'application/json')
         if parsed_envelope.is_base64_encoded:
-            body = json.loads(base64.b64decode(parsed_envelope.body).decode())
+            body = base64.b64decode(parsed_envelope.body).decode()
+            if content_type == 'application/json':
+                body = json.loads(body)
+            elif content_type == 'multipart/form-data':
+                mp_data = decoder.MultipartDecoder(body, 'multipart/form-data')
+                body = {}
+                for part in mp_data.parts:
+                    print(part['headers']['content-disposition'])
         else:
             body = parsed_envelope.body
 
