92be9e993c4df755f48b165f4351bd16e8ce4db7
Maksymilian Babarowski <maksymilian.babarowski@gmail.com>
2021-04-30 23:23:33 +0200

    Redirect user to main page from /signup if already authenticated

--- front/src/views/SignUp.tsx
+++ front/src/views/SignUp.tsx
@@ -2,15 +2,23 @@ import React, {ReactElement, useState} from 'react';
 import {SignUpParams} from '../models/UI';
 import SignUpForm from '../components/signup/SignUpForm';
 import ConfirmationForm from '../components/signup/ConfirmationForm';
+import {useAppContext} from '../utils/contextLib';
+import {Redirect} from 'react-router-dom';
 
 const SignUp: React.FC = (): ReactElement => {
+
   const [newUser, setNewUser] = useState<SignUpParams | undefined>(undefined);
+  const {isAuthenticated} = useAppContext();
 
   return (
     <>
-      {newUser == undefined ?
-        <SignUpForm setNewUser={setNewUser} /> :
-        <ConfirmationForm newUser={newUser} />
+      {isAuthenticated ?
+        <Redirect to='/' /> :
+        (
+          newUser == undefined ?
+            <SignUpForm setNewUser={setNewUser} /> :
+            <ConfirmationForm newUser={newUser} />
+        )
       }
     </>
   );
