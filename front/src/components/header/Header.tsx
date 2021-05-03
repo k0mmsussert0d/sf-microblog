988be3cadfac7ded74a7cac497dff0c50b391bbb
Maksymilian Babarowski <maksymilian.babarowski@gmail.com>
2021-05-03 22:39:06 +0200

    Added icons to menu buttons

--- front/src/components/header/Header.tsx
+++ front/src/components/header/Header.tsx
@@ -1,9 +1,11 @@
-import { Navbar, Button } from 'rbx';
+import {Navbar, Button, Icon} from 'rbx';
 import React, {ReactElement} from 'react';
 import './Header.scss';
 import {Link, Redirect} from 'react-router-dom';
 import {useAppContext} from '../../utils/contextLib';
 import {Auth} from 'aws-amplify';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faPlus, faSignOutAlt, faUser} from '@fortawesome/free-solid-svg-icons';
 
 
 const Header: React.FC = (): ReactElement => {
@@ -41,17 +43,30 @@ const Header: React.FC = (): ReactElement => {
     return (
       <Navbar.Menu>
         <Navbar.Segment align="start">
-          <Navbar.Item as={Link} to='/'>
-              Home
+          <Navbar.Item>
+            <Button.Group>
+              <Button color='light'>
+                <Icon size='small'>
+                  <FontAwesomeIcon icon={faPlus} />
+                </Icon>
+                <span>Add new post</span>
+              </Button>
+            </Button.Group>
           </Navbar.Item>
         </Navbar.Segment>
         <Navbar.Segment align="end">
           <Navbar.Item>
             <Button.Group>
               <Button color='light' as={Link} to='/profile'>
-                Profile
+                <Icon>
+                  <FontAwesomeIcon icon={faUser} />
+                </Icon>
+                <span>Profile</span>
               </Button>
               <Button color="primary" onClick={performLogout}>
+                <Icon>
+                  <FontAwesomeIcon icon={faSignOutAlt} />
+                </Icon>
                 <strong>Log out</strong>
               </Button>
             </Button.Group>
