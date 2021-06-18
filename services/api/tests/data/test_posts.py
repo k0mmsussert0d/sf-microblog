44e375c4f702500de82386a0c32a197a346ef40b
Maksymilian Babarowski <maksymilian.babarowski@gmail.com>
2021-06-18 20:16:27 +0200

    tests: Covered get_post_doc function

--- services/api/tests/data/test_posts.py
+++ services/api/tests/data/test_posts.py
@@ -1,5 +1,35 @@
 from unittest import TestCase
 
+from microblog.models.db import PostDoc
+
+
+def test__get_post_doc__if_post_does_not_exist__return_none(
+        dynamodb_posts_table
+):
+    from microblog.data.posts import get_post_doc
+
+    res = get_post_doc(1)
+    assert res is None
+
+
+def test__get_post_doc__if_post_exists__return_post(
+        test_data,
+        dynamodb_posts_table
+):
+    from microblog.data.posts import get_post_doc
+    from microblog.utils.clients import posts_table
+
+    post = test_data['posts'][0]
+    post_db_format = {**post, 'active': 1 if post['active'] else 0}
+    posts_table().put_item(
+        Item=post_db_format
+    )
+
+    res = get_post_doc(post['id'])
+
+    assert res is not None
+    assert res == PostDoc.parse_obj(post_db_format)
+
 
 def test_get_user_posts__if_user_has_posts__return_all_active_posts(
         test_data,
